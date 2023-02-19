03d5f798e445fbcd408a12c8e55a4ddf52076a10
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-02-19 19:40:25 +0100

    Missplaced commit.

--- my-app/src/sections/hero/hero.js
+++ my-app/src/sections/hero/hero.js
@@ -3,9 +3,22 @@ import './hero.scss';
 
 function Hero() {
   return (
-    <div className='section-wrapper'>
-      
-    </div>
+    <section className='section-wrapper'>
+      <h2>Popular now!</h2>
+      <div className='games-container'>
+        <div className='game'>
+          <img className='game-img'></img>
+          <div className='information'>
+            <div className='text'>
+              <div className='name'>
+                <span className='title'>Ghost of Tushima</span>
+              </div>
+              <div className='price'>200.00zł</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 

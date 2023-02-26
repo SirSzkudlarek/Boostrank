df144697e3dd87ff3bd6f305a01e8f2093de0ac6
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-02-26 03:49:08 +0100

    Icons section done

--- my-app/src/sections/footer/footer.js
+++ my-app/src/sections/footer/footer.js
@@ -2,16 +2,66 @@ import './footer.scss';
 
 function Footer() {
   return (
-    <div className='footer-wrapper'>
-      <nav className='nav-wrapper'>
-        <ul className='nav'>
-          <li className='nav-item'>Donate</li>
-          <li className='nav-item'>Adopt</li>
-          <li className='nav-item'>Kontakt</li>
-          <li className='nav-item'>Facebook</li>
-        </ul>
-      </nav>
-    </div>
+    <footer className='footer-wrapper'>
+      <div className='footer-content'>
+        <div className='links'>
+          <div className='logo-list'>
+            <div className='logo'>
+              <a href="#" title="Homepage" className='logo-br'>Boostrank</a>
+            </div>
+            <ul className='list'>
+              <li>
+                <a className='sublinks'>Polityka prywatności</a>
+              </li>
+              <li>
+                <a className='sublinks'>Warunki korzystania</a>
+              </li>
+              <li>
+                <a className='sublinks'>Kontakt do nas</a>
+              </li>
+              <li>
+                <a className='sublinks'>FAQ</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='media'>
+            <div className='icons'>
+              {/* Facebook */}
+              <a href="#" title='Facebook' className='socials facebook'>
+                <div className='icon-facebook icon-socials'></div>
+              </a>
+
+              {/* Twitch */}
+              <a href="#" title='Twitch' className='socials twitch'>
+                <div className='icon-twitch icon-socials'></div>
+              </a>
+
+              {/* Youtube */}
+              <a href="#" title='Youtube' className='socials youtube'>
+                <div className='icon-youtube icon-socials'></div>
+              </a>
+
+              {/* Instagram */}
+              <a href="#" title='Instagram' className='socials instagram'>
+                <div className='icon-instagram icon-socials'></div>
+              </a>
+
+              {/* Reddit */}
+              <a href="#" title='Reddit' className='socials reddit'>
+                <div className='icon-reddit icon-socials'></div>
+              </a>
+
+              {/* Twitter */}
+              <a href="#" title='Twitter' className='socials twitter'>
+                <div className='icon-twitter icon-socials'></div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className='information'></div>
+      </div>
+    </footer>
   );
 }
 

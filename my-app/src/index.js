be57ba077a6ed49e3768e80453c11514e8625381
Damian Szkudlarek <damian.szkudlarek@yuush.com>
2023-04-22 21:57:05 +0200

    Slidner done for all diveces.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,6 +7,7 @@ import TrendyGames from './sections/trendyGames/trendyGames';
 import TrustPanel from './sections/trustPanel/trustPanel';
 import Preorders from './sections/preorders/preorders';
 import Partner from './sections/partner/partner';
+import SliderReviews from './sections/sliderReviews/sliderReviews';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,6 +19,7 @@ root.render(
       <TrustPanel/>
       <Preorders/>
       <Partner/>
+      <SliderReviews/>
     </Layout>
   </React.StrictMode>
 );
